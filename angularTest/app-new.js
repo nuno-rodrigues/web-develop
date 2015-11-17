5d935fafa7d2a9658944222e3c1a185a5811d22e
Nuno Rodrigues <nuno.rodrigues@vision-box.com>
2015-11-17 11:02:27 +0000

    Update directory

---
+++ angularTest/app-new.js
@@ -0,0 +1,28 @@
+﻿angular.module('app', ['ngRoute']);
+
+var app = angular.module ('store', ['store-products']);
+
+app.controller('StoreController', ['$http', function($http){
+
+	var store = this;
+	
+	store.products = [];
+	
+	$http.get('products.json').success(function(data){
+		
+		store.products = data;
+		
+	});
+	
+}]);
+
+app.controller('ReviewController', function(){
+	
+	this.review = {};
+	
+	this.addReview = function(product){
+		this.review.createdOn = Date.now();
+		product.reviews.push(this.review);
+		this.review = {};
+	};
+});
